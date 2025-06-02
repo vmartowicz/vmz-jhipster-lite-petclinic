f2ddc8d358bc3ad0e78c2c43594939b51c8aa109
Vincent MARTOWICZ <vmartowicz@gmail.com>
2025-06-02 10:24:28 +0200

    Apply module: spring-boot-oauth2

--- src/test/java/com/petclinic/IntegrationTest.java
+++ src/test/java/com/petclinic/IntegrationTest.java
@@ -1,5 +1,6 @@
 package com.petclinic;
 
+import com.petclinic.shared.authentication.infrastructure.primary.TestSecurityConfiguration;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -7,13 +8,15 @@ import java.lang.annotation.Target;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.annotation.AliasFor;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
+@WithMockUser
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @DisplayNameGeneration(ReplaceCamelCase.class)
-@SpringBootTest(classes = { JhipetclinicApp.class })
+@SpringBootTest(classes = { JhipetclinicApp.class, TestSecurityConfiguration.class })
 public @interface IntegrationTest {
   @AliasFor(annotation = SpringBootTest.class)
   String[] properties() default {};
