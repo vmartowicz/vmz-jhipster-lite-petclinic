5b41091652bd0fae37f9795d0b81e9c060a437b3
Vincent MARTOWICZ <vmartowicz@gmail.com>
2025-06-05 00:45:04 +0200

    Apply module: cypress-component-tests

--- src/test/webapp/component/cypress-config.ts
+++ src/test/webapp/component/cypress-config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:9002',
+    baseUrl: 'http://localhost:9000',
     specPattern: 'src/test/webapp/component/**/*.(spec|cy).ts',
     fixturesFolder: false,
     supportFile: false,
